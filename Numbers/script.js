84855d2ff087c70bf793de193657e8837ab540cb
Florin Harabagiu <harabagiufl@gmail.com>
2019-12-04 18:21:37 +0200

    Added Fibonacci Sequence

--- Numbers/script.js
+++ Numbers/script.js
@@ -2,30 +2,77 @@
 function bin2dec()
 {
     var binaryValue = 
-    document.getElementById("bin2dec-input").value;
+    document.getElementById("std-input-text").value;
     var regex = RegExp("\\b[01]+\\b");
     if (regex.test(binaryValue) == false)
     {
-        document.getElementById("bin2dec-error").innerHTML = "Please enter a valid binary number! (Only 1s and 0s)" 
+        document.getElementById("std-error").innerHTML = "Please enter a valid binary number! (Only 1s and 0s)" 
         return;
     }
-    document.getElementById("bin2dec-error").innerHTML = "";
+    document.getElementById("std-error").innerHTML = "";
     var result = 0;
     for (var i = 0; i < binaryValue.length; i++)
     {
         result = result + (binaryValue[i] * Math.pow(2, (binaryValue.length - i - 1)));
     }
-    document.getElementById("bin2dec-output").value = result.toString();    
+    document.getElementById("std-output-text").value = result.toString();    
 }
 function dec2bin()
 {
     var decimalValue = 
-    document.getElementById("dec2bin-input").value;
+    document.getElementById("std-input-number").value;
     var result = "";
     while(decimalValue >= 1)
     {
         result = (decimalValue % 2).toString() + result;
         decimalValue = Math.trunc(decimalValue / 2);
     }
-    document.getElementById("dec2bin-output").value = result;
+    document.getElementById("std-output-text").value = result;
+}
+function fibSequence()
+{
+    var fibTerm = 
+    document.getElementById("std-input-text").value;
+    document.getElementById("std-output-paragraph").innerHTML = "";
+    var text = "";
+    for (var i = 1; i <= fibTerm; i++)
+    {
+        text += calculateFib(i) + " ";
+    }
+    var speed = 75;
+    var i = 0;
+    var breakPoint = 45;
+    function typeWriter()
+    {
+        if (i < text.length)
+        {
+            document.getElementById("std-output-paragraph").innerHTML += text.charAt(i);
+            i++;
+            if (i % breakPoint == 0)
+            {
+                document.getElementById("std-output-paragraph").innerHTML += "<br>";
+                if (speed >= 30) speed -= 15;
+            }
+            setTimeout(typeWriter, speed);
+        }
+    }
+    typeWriter();
+}
+function calculateFib(n, prevValues = [])
+{
+    if (prevValues[n] != null)
+    {
+        return prevValues[n];
+    }
+    var result;
+    if (n <= 2)
+    {
+        result = 1;
+    }
+    else
+    {
+        result = calculateFib(n - 1, prevValues) + calculateFib (n - 2, prevValues);
+    }
+    prevValues[n] = result;
+    return result;
 }
